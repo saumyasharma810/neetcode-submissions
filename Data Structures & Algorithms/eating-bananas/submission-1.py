1b06792831408edcece0055325555abf8f77b719
Saumya Sharma <99022565+saumyasharma810@users.noreply.github.com>
2026-04-16 18:50:13 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def canBeDone(self, x: int, h: int, piles: List[int]) -> bool:
+        hours = 0
+        for pile in piles:
+            hours += pile//x if pile%x == 0 else (pile//x) + 1
+        return True if hours <= h else False
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l,r = 1, sum(piles)
+        while l<r:
+            mid = l + (r-l)//2
+            print(l,r,mid)
+            if self.canBeDone(mid,h,piles):
+                r = mid
+            else:
+                l = mid+1
+        print(l,r)
+        return l if self.canBeDone(l,h,piles) else r
