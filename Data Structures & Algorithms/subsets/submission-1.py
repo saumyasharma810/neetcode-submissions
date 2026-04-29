89c1158b00e534fe35250535764b8bb1bb344d8a
Saumya Sharma <99022565+saumyasharma810@users.noreply.github.com>
2026-04-29 16:18:58 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subset_lst = [[]]
+        for i in range(len(nums)):
+            new_subset_lst = []
+            for subset in subset_lst:
+                subset_copy = []
+                if subset:
+                    subset_copy = subset[:]
+                subset_copy.append(nums[i])
+                new_subset_lst.append(subset)
+                new_subset_lst.append(subset_copy)
+            subset_lst = new_subset_lst
+        return subset_lst
+
+        
